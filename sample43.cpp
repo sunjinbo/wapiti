b7cb8b0007f9df708522da74a233541dd4e24afa
sunjinbo <sunjinbo@360.cn>
2021-08-27 21:07:14 +0800

    mutex

---
+++ sample43.cpp
@@ -0,0 +1,93 @@
+// 并行处理
+
+#include <iostream>
+#include <thread>
+#include <future>
+#include <queue>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+int v = 1;
+
+void critical_section(int change_v) {
+    static std::mutex mtx;
+    std::lock_guard<std::mutex> lock(mtx);
+
+    // 执行竞争操作
+    v = change_v;
+
+    // 离开此作用域后 mtx 会被释放
+}
+
+int main() {
+    std::thread t([](){
+        std::cout << "hello thread." << std::endl;
+    });
+    t.join();
+
+    std::thread t1(critical_section, 2), t2(critical_section, 3);
+    t1.join();
+    t2.join();
+
+    std::cout << v << std::endl;
+
+    // 将一个返回值为7的 lambda 表达式封装到 task 中
+    // std::packaged_task 的模板参数为要封装函数的类型
+    std::packaged_task<int()> task([](){return 7;});
+    // 获得 task 的期物
+    std::future<int> result = task.get_future(); // 在一个线程中执行 task
+    std::thread(std::move(task)).detach();
+    std::cout << "waiting...";
+    result.wait(); // 在此设置屏障，阻塞到期物的完成
+    // 输出执行结果
+    std::cout << "done!" << std:: endl << "future result is " << result.get() << std::endl;
+
+    std::queue<int> produced_nums;
+    std::mutex mtx;
+    std::condition_variable cv;
+    bool notified = false;  // 通知信号
+
+    // 生产者
+    auto producer = [&]() {
+        for (int i = 0; ; i++) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(900));
+            std::unique_lock<std::mutex> lock(mtx);
+            std::cout << "producing " << i << std::endl;
+            produced_nums.push(i);
+            notified = true;
+            cv.notify_all(); // 此处也可以使用 notify_one
+        }
+    };
+    // 消费者
+    auto consumer = [&]() {
+        while (true) {
+            std::unique_lock<std::mutex> lock(mtx);
+            while (!notified) {  // 避免虚假唤醒
+                cv.wait(lock);
+            }
+            // 短暂取消锁，使得生产者有机会在消费者消费空前继续生产
+            lock.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // 消费者慢于生产者
+            lock.lock();
+            while (!produced_nums.empty()) {
+                std::cout << "consuming " << produced_nums.front() << std::endl;
+                produced_nums.pop();
+            }
+            notified = false;
+        }
+    };
+
+    // 分别在不同的线程中运行
+    std::thread p(producer);
+    std::thread cs[2];
+    for (int i = 0; i < 2; ++i) {
+        cs[i] = std::thread(consumer);
+    }
+    p.join();
+    for (int i = 0; i < 2; ++i) {
+        cs[i].join();
+    }
+
+    return 0;
+}
